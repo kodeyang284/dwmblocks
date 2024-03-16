8bdc53cf3878cf6865ceaf7b8000d629bb796ecd
kodeyang <1468466525@qq.com>
2024-03-16 18:15:43 +0800

    fist commit

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
+
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "db-mem",	      10,		6},
+	{"", "db-disk",		    50,		7},
+	{"", "db-cputmp",			10,		5},
+	{"", "db-battery",		30,		2},
+	{"", "db-internet",		5,		3},
+	{"", "db-volum",			1,		4},
+	{"", "db-clock",			30,		1},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
